5c53267c600608f3f7f188b56571b584cb4049fc
xAtegox <thegamingrene@gmail.com>
2025-12-27 12:12:55 +0100

    tried fixin but didnt work

--- config.h
+++ config.h
@@ -1,13 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	// {"", "~/.config/scripts/statusbar/colorscheme",			0,		8},
-	{"", "~/.config/scripts/statusbar/screenrecord",			1,		12},
-	{"", "~/.config/scripts/statusbar/musicplaying",			0,		11},
-	{"", "~/.config/scripts/statusbar/weather",				1200,		0},
-	{"", "~/.config/scripts/statusbar/wifi",				30,		0},
-	{"", "~/.config/scripts/statusbar/battery",				120,		0},
-	{"", "~/.config/scripts/statusbar/timedate",			60,		0},
+	/*Icon*/    /*Command*/                                           /*Update Interval*/ /*Update Signal*/
+	{"",        "/home/atego/.config/scripts/screenrecord status",    30,                12},
+	{"",        "/home/atego/.config/scripts/statusbar/weather",      600,                0},
+	{"",        "/home/atego/.config/scripts/statusbar/wifi",         30,                 0},
+	{"",        "/home/atego/.config/scripts/statusbar/battery",      120,                0},
+	{"",        "/home/atego/.config/scripts/statusbar/timedate",     60,                 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
