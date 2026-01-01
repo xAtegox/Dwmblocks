a503080d00cc9d61c2197b58c9456c652c812bfc
xAtegox <thegamingrene@gmail.com>
2026-01-01 16:21:28 +0100

    fixed dwmblocks

--- config.h
+++ config.h
@@ -1,11 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/    /*Command*/                                           /*Update Interval*/ /*Update Signal*/
-	{"",        "/home/atego/.config/scripts/screenrecord status",    30,                12},
-	{"",        "/home/atego/.config/scripts/statusbar/weather",      600,                0},
-	{"",        "/home/atego/.config/scripts/statusbar/wifi",         30,                 0},
-	{"",        "/home/atego/.config/scripts/statusbar/battery",      120,                0},
-	{"",        "/home/atego/.config/scripts/statusbar/timedate",     60,                 0},
+
+  /* Only show WiFi if LAN (ethernet) is NOT connected */
+  {"", "[ $(cat /sys/class/net/e*/carrier 2>/dev/null | grep -c 1) -eq 0 ] && /home/atego/dotfiles/scripts/.config/scripts/statusbar/wifi", 30, 0},
+
+  /* Only show Battery if the BAT0 directory exists */
+  {"", "[ -d /sys/class/power_supply/BAT0 ] && /home/atego/dotfiles/scripts/.config/scripts/statusbar/battery", 60, 0},
+
+  /* Deafult scripts */
+  {"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/musicplaying",  30,               11},
+  {"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/screenrecord",   0,               12},
+	{"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/weather",      600,               15},
+	{"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/volume",       300,               10},
+	{"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/timedate",      30,                0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
