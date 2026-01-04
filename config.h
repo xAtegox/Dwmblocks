8d27bf674fa599d72bbd8b672f61bb2e3cc58cda
xAtegox <thegamingrene@gmail.com>
2026-01-04 12:51:43 +0100

    Automatic Upload

--- config.h
+++ config.h
@@ -3,16 +3,18 @@ static const Block blocks[] = {
 	/*Icon*/    /*Command*/                                           /*Update Interval*/ /*Update Signal*/
 
   /* Only show WiFi if LAN (ethernet) is NOT connected */
-  {"", "[ $(cat /sys/class/net/e*/carrier 2>/dev/null | grep -c 1) -eq 0 ] && /home/atego/dotfiles/scripts/.config/scripts/statusbar/wifi", 30, 0},
+
 
   /* Only show Battery if the BAT0 directory exists */
-  {"", "[ -d /sys/class/power_supply/BAT0 ] && /home/atego/dotfiles/scripts/.config/scripts/statusbar/battery", 60, 0},
+ 
 
   /* Deafult scripts */
-  {"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/musicplaying",  10,               11},
+  {"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/musicplaying",   0,               11},
   {"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/screenrecord",   0,               12},
 	{"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/weather",      180,               15},
+  {"", "[ $(cat /sys/class/net/e*/carrier 2>/dev/null | grep -c 1) -eq 0 ] && /home/atego/dotfiles/scripts/.config/scripts/statusbar/wifi", 30, 0},
 	{"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/volume",         0,               10},
+  {"", "[ -d /sys/class/power_supply/BAT0 ] && /home/atego/dotfiles/scripts/.config/scripts/statusbar/battery", 60, 0},
 	{"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/timedate",      30,                0},
 };
 
