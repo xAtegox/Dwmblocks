4e74db40e0a87890d2fad84be8b3d2ba87d40cbf
xAtegox <thegamingrene@gmail.com>
2026-03-15 17:10:29 +0100

    change

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+#define HOME "/home/atego"
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+static const Block blocks[] = {
+    /*Icon*/    /*Command*/                                                                           /*Update Interval*/ /*Update Signal*/
+
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/musicplaying",   0,                11},
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/screenrecord",   0,                12},
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/weather",      180,                15},
+    /* For the inline shell checks, you break the string just like you did for SHCMD in dwm */
+    {"", "[ $(cat /sys/class/net/e*/carrier 2>/dev/null | grep -c 1) -eq 0 ] && " HOME "/dotfiles/scripts/.config/scripts/statusbar/wifi", 30, 0},
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/volume",           0,                10},
+    {"", "[ -d /sys/class/power_supply/BAT0 ] && " HOME "/dotfiles/scripts/.config/scripts/statusbar/battery", 60, 0},
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/timedate",       30,                 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 3;
--- config.h
+++ config.h
@@ -1,21 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
+#define HOME "/home/atego"
+
 static const Block blocks[] = {
-	/*Icon*/    /*Command*/                                           /*Update Interval*/ /*Update Signal*/
+    /*Icon*/    /*Command*/                                                                           /*Update Interval*/ /*Update Signal*/
 
-  /* Only show WiFi if LAN (ethernet) is NOT connected */
-
-
-  /* Only show Battery if the BAT0 directory exists */
- 
-
-  /* Deafult scripts */
-  {"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/musicplaying",   0,               11},
-  {"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/screenrecord",   0,               12},
-	{"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/weather",      180,               15},
-  {"", "[ $(cat /sys/class/net/e*/carrier 2>/dev/null | grep -c 1) -eq 0 ] && /home/atego/dotfiles/scripts/.config/scripts/statusbar/wifi", 30, 0},
-	{"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/volume",         0,               10},
-  {"", "[ -d /sys/class/power_supply/BAT0 ] && /home/atego/dotfiles/scripts/.config/scripts/statusbar/battery", 60, 0},
-	{"",        "/home/atego/dotfiles/scripts/.config/scripts/statusbar/timedate",      30,                0},
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/musicplaying",   0,                11},
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/screenrecord",   0,                12},
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/weather",      180,                15},
+    /* For the inline shell checks, you break the string just like you did for SHCMD in dwm */
+    {"", "[ $(cat /sys/class/net/e*/carrier 2>/dev/null | grep -c 1) -eq 0 ] && " HOME "/dotfiles/scripts/.config/scripts/statusbar/wifi", 30, 0},
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/volume",           0,                10},
+    {"", "[ -d /sys/class/power_supply/BAT0 ] && " HOME "/dotfiles/scripts/.config/scripts/statusbar/battery", 60, 0},
+    {"",        HOME "/dotfiles/scripts/.config/scripts/statusbar/timedate",       30,                 0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
